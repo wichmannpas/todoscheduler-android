7c85b0cd3703a2f6e7cbbbbb780ed970e9a6c834
Pascal Wichmann <git@pa-w.de>
2018-08-28 17:38:03 +0200

    Open todoscheduler.org in web view

--- app/src/main/java/org/todoscheduler/todoscheduler/MainActivity.java
+++ app/src/main/java/org/todoscheduler/todoscheduler/MainActivity.java
@@ -1,13 +1,44 @@
 package org.todoscheduler.todoscheduler;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new TodoSchedulerWebViewClient());
+
+        webView.loadUrl(getString(R.string.client_url));
+    }
+
+    private class TodoSchedulerWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("todoscheduler.org")) {
+                return false;
+            }
+
+            // use external browser for external links
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
